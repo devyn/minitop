9c3453c3c61dd958d64dab4f61f3f6dffdfce830
devyn <devyn.cairns@gmail.com>
2008-10-24 16:27:31 -0700

    okay, everything works now, including parts

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,7 @@ class MainController < ApplicationController
     layout 'main'
     def index
         @links = Link.find(:all)
+        @parts = Part.find(:all)
     end
     def add
         Link.create(:name => params[:name], :url => params[:url])
@@ -10,5 +11,9 @@ class MainController < ApplicationController
     def remove
         Link.destroy(params[:id])
         redirect_to :action => 'index'
+    end
+    def part
+        p = Part.find(params[:id])
+        render :inline => p.template, :layout => true
     end
 end
