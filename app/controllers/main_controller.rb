100829ccc6ad9e5672a1590bae2aee47d1ef2432
devyn <devyn.cairns@gmail.com>
2008-10-24 12:57:41 -0700

    controller for adding links

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,4 +3,8 @@ class MainController < ApplicationController
     def index
         @links = Links.find(:all)
     end
+    def add
+        Links.create(:name => params[:name], :url => params[:url])
+        redirect_to :action => 'index'
+    end
 end
